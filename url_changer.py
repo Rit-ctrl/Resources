80d6332f1be0e5a91548ddb84d04b78fcdc6fb51
Santhosh Raghul <gssanthoshraghul1010@gmail.com>
2021-02-23 01:32:50 +0530

    update url_changer.py - invoke function for all md files

--- url_changer.py
+++ url_changer.py
@@ -1,38 +1,36 @@
-from os import remove
-
+from os import remove,listdir
 
 def convert(content):
-    if('open?id=' not in content):
-        return content
-    [text, url] = content.split('(')
-    url = url.split(')')[0]
-    [x, y] = url[0:-11].split('open?id=')
-    ret = text+'('+x+'file/d/'+y+'/view)'
-    return ret
-
+	if('open?id=' not in content):
+		return content
+	[text, url] = content.split('(')
+	url = url.split(')')[0]
+	[x, y] = url[0:-11].split('open?id=')
+	ret = text+'('+x+'file/d/'+y+'/view)'
+	return ret
 
 def change_url(filename):
 
-    f = open(filename, "r")
-    lines = f.readlines()
-    f.close()
-    remove(filename)
-    new = []
+	f = open(filename, "r")
+	lines = f.readlines()
+	f.close()
+	remove(filename)
+	new = []
 
-    for line in lines:
-        a = line.split('|')
-        if(len(a) == 1):
-            new.append(line)
-            continue
-        a[3] = convert(a[3])
-        a[4] = convert(a[4])
-        new.append('|'.join(a))
+	for line in lines:
+		a = line.split('|')
+		if(len(a) == 1):
+			new.append(line)
+			continue
+		a[3] = convert(a[3])
+		a[4] = convert(a[4])
+		new.append('|'.join(a))
 
-    f = open(filename, "w")
-    f.writelines(new)
-    f.close()
-
+	f = open(filename, "w")
+	f.writelines(new)
+	f.close()
 
 if __name__ == "__main__":
-    change_url("Markdown/CN.md")
-    change_url("Markdown/OS.md")
+	for file_ in listdir("Markdown/"):
+		if file_.endswith(".md"):
+			change_url("Markdown/"+file_)
